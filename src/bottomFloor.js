faffb8e7862192eba83df9f1acec492a02c85515
WairenD <WairenD@users.noreply.github.com>
2021-12-19 17:01:25 +0100

    bottom floor finish

---
+++ src/bottomFloor.js
@@ -0,0 +1,43 @@
+var inventory = [];
+var catRemoved = false;
+let txt = "";
+
+function addItem(item){
+  if(!inventory.includes(item) || inventory == null){
+      inventory.push(item);
+      sessionStorage.setItem('inventory', inventory);
+
+      inventory.forEach(getInventory);
+      document.getElementById("inventory").innerHTML = txt;
+      txt = "";
+  }
+}
+function inventoryIndexOf(number){
+  inventory = sessionStorage.getItem('inventory');
+  inventory = inventory.split(',');
+  alert(inventory[number]);
+}
+
+function removeCat(){
+  if(catRemoved==false){
+    document.getElementById('imageContainer').style.backgroundImage ="url('../../../assets/images/house-no-cat.png')";
+    document.getElementById('sfxJoJo').style.opacity = "0";
+    catRemoved = true;
+  }else{
+    document.getElementById('imageContainer').style.backgroundImage ="url('../../../assets/images/house.png')";
+    document.getElementById('sfxJoJo').style.opacity = "1";
+    catRemoved = false;
+  }
+
+}
+function openBox(){
+  if(catRemoved){
+    addItem('hazmatSuit');
+  }
+}
+function goToGroundFloor(){
+  // TODO:
+}
+function getInventory(value, index, array) {
+  txt += value + " ";
+}
