5cc393cb34941d096f6656ef0f3ae700f52ba081
Joel Kleibs <joel.kleibs@student.nhlstenden.com>
2021-12-20 13:32:40 +0100

    architecture update

--- src/scripts.js
+++ src/scripts.js
@@ -2,43 +2,65 @@ var inventory = [];
 var catRemoved = false;
 let txt = "";
 
-if()
 function addItem(item){
-  if(!inventory.includes(item) || inventory == null){
-      inventory.push(item);
-      sessionStorage.setItem('inventory', inventory);
+    if(!inventory.includes(item) || inventory == null){
+        inventory.push(item);
+        sessionStorage.setItem('inventory', inventory);
 
-      inventory.forEach(getInventory);
-      document.getElementById("inventory").innerHTML = txt;
-      txt = "";
-  }
+        inventory.forEach(getInventory);
+        document.getElementById("inventory").innerHTML = txt;
+        txt = "";
+    }
 }
 function inventoryIndexOf(number){
-  inventory = sessionStorage.getItem('inventory');
-  inventory = inventory.split(',');
-  alert(inventory[number]);
+    inventory = sessionStorage.getItem('inventory');
+    inventory = inventory.split(',');
+    alert(inventory[number]);
 }
 
 function removeCat(){
-  if(catRemoved==false){
-    document.getElementById('imageContainer').style.backgroundImage ="url('../../../assets/images/house-no-cat.png')";
-    document.getElementById('sfxJoJo').style.opacity = "0";
-    catRemoved = true;
-  }else{
-    document.getElementById('imageContainer').style.backgroundImage ="url('../../../assets/images/house.png')";
-    document.getElementById('sfxJoJo').style.opacity = "1";
-    catRemoved = false;
-  }
-
+    if(catRemoved==false){
+        document.getElementById('imageContainer').style.backgroundImage ="url('../../../assets/images/house-no-cat.png')";
+        document.getElementById('sfxJoJo').style.opacity = "0";
+        catRemoved = true;
+    }else{
+        document.getElementById('imageContainer').style.backgroundImage ="url('../../../assets/images/house.png')";
+        document.getElementById('sfxJoJo').style.opacity = "1";
+        catRemoved = false;
+    }
+    sessionStorage.setItem('catRemoved', catRemoved);
 }
 function openBox(){
-  if(catRemoved){
-    addItem('hazmatSuit');
-  }
+    if(catRemoved){
+        addItem('hazmatSuit');
+    }
 }
 function goToGroundFloor(){
-  // TODO:
+    console.log(sessionStorage.getItem('catRemoved'));
+    if(sessionStorage.getItem('catRemoved')){
+        window.location.href = "./groundFloor.html";
+        document.getElementById('imageContainer').style.backgroundImage = "url('../../../assets/gameStates/no cat/start-noCat-keys.png')";
+    }else{
+        window.location.href = "./groundFloor.html";
+        document.getElementById('imageContainer').style.backgroundImage = "url('../../../assets/gameStates/start-cat-keys.png')";
+    }
+}
+function goToBottomFloor(){
+    location.assign("./bottomFloor/bottomFloor.html");
+    window.location.href = "./bottomFloor.html";
+}
+function goToTopFloor(){
+    inventory = sessionStorage.getItem('inventory');
+    if(inventory != null){
+        if(inventory.includes("hazmatSuit")){
+            window.location.href = "./topFloor.html";
+        }else{
+            alert("You gonna die bitch!")
+        }
+    }else{
+        alert("You gonna die bitch!")
+    }
 }
 function getInventory(value, index, array) {
-  txt += value + " ";
+    txt += value + " ";
 }
