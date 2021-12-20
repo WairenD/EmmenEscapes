2f2bd6cf2c43c57d8434c4c444fe3934472817cf
JeremiHaakmat <jeremi.haakmat@student.nhlstenden.com>
2021-12-20 18:30:21 +0100

    started garage slider
    renamed openBox to openPopup

--- src/scripts.js
+++ src/scripts.js
@@ -9,7 +9,7 @@ function addItem(item) {
         var element = document.createElement('img');
         element.setAttribute("id", item);
         document.getElementById('inventory').appendChild(element);
-        document.getElementById(item).src="../../assets/images/"+item+".png";
+        document.getElementById(item).src = "../../assets/images/" + item + ".png";
         document.getElementById(item).style.marginRight = "2rem";
         document.getElementById(item).style.marginTop = "2rem";
     }
@@ -20,6 +20,7 @@ function inventoryIndexOf(number){
     alert(inventory[number]);
 }
 
+<<<<<<< Updated upstream
 function removeCat(){
     if(catRemoved==false){
         document.getElementById('jojoSFX').style.opacity = "0";
@@ -28,12 +29,28 @@ function removeCat(){
     }else{
         document.getElementById('jojoSFX').style.opacity = "1";
         document.getElementById('cat').style.opacity = "1";
+=======
+function removeCat() {
+    if (catRemoved == false) {
+        document.getElementById('imageContainer').style.backgroundImage = "url('../../assets/gameStates/no cat no keys/bottomFloor-noCat-noKeys.png')";
+        document.getElementById('sfxJoJo').style.opacity = "0";
+        catRemoved = true;
+    } else {
+        document.getElementById('imageContainer').style.backgroundImage = "url('../../assets/gameStates/no keys/bottomFloor-cat-noKeys.png')";
+        document.getElementById('sfxJoJo').style.opacity = "1";
+>>>>>>> Stashed changes
         catRemoved = false;
     }
     localStorage.setItem('catRemoved', catRemoved);
 }
+<<<<<<< Updated upstream
 function openBox(){
     if(catRemoved){
+=======
+
+function openBox() {
+    if (catRemoved) {
+>>>>>>> Stashed changes
         addItem('gasSuit');
     }
 }
@@ -63,6 +80,13 @@ function goToTopFloor(){
         alert("You gonna die bitch!")
     }
 }
+<<<<<<< Updated upstream
+=======
+
+function getInventory(value, index, array) {
+    txt += value + " ";
+}
+>>>>>>> Stashed changes
 
 var password = "21706694";
 var insertedPassword = "";
@@ -98,28 +122,116 @@ function reset() {
     buttonClickSound.play();
 }
 
+function swapTiles(cell1, cell2) {
+    var temp = document.getElementById(cell1).className;
+    document.getElementById(cell1).className = document.getElementById(cell2).className;
+    document.getElementById(cell2).className = temp;
+}
+
+function shuffle() {
+    //Use nested loops to access each cell of the 3x3 grid
+    for (var row = 1; row <= 3; row++) { //For each row of the 3x3 grid
+        for (var column = 1; column <= 3; column++) { //For each column in this row
+
+            var row2 = Math.floor(Math.random() * 3 + 1); //Pick a random row from 1 to 3
+            var column2 = Math.floor(Math.random() * 3 + 1); //Pick a random column from 1 to 3
+
+            swapTiles("cell" + row + column, "cell" + row2 + column2); //Swap the look & feel of both cells
+        }
+    }
+}
+
+function clickTile(row, column) {
+    var cell = document.getElementById("cell" + row + column);
+    var tile = cell.className;
+    if (tile != "tile9") {
+        //Checking if white tile on the right
+        if (column < 3) {
+            if (document.getElementById("cell" + row + (column + 1)).className == "tile9") {
+                swapTiles("cell" + row + column, "cell" + row + (column + 1));
+                return;
+            }
+        }
+        //Checking if white tile on the left
+        if (column > 1) {
+            if (document.getElementById("cell" + row + (column - 1)).className == "tile9") {
+                swapTiles("cell" + row + column, "cell" + row + (column - 1));
+                return;
+            }
+        }
+        //Checking if white tile is above
+        if (row > 1) {
+            if (document.getElementById("cell" + (row - 1) + column).className == "tile9") {
+                swapTiles("cell" + row + column, "cell" + (row - 1) + column);
+                return;
+            }
+        }
+        //Checking if white tile is below
+        if (row < 3) {
+            if (document.getElementById("cell" + (row + 1) + column).className == "tile9") {
+                swapTiles("cell" + row + column, "cell" + (row + 1) + column);
+                return;
+            }
+        }
+    }
+
+}
 
 // Get the <span> element that closes the modal
 
 
+<<<<<<< Updated upstream
 // When the user clicks the button, open the modal
 function openBox() {
     document.getElementById("numberpadContainer").style.display = "block";
     console.log("modal has been sucessfully opened");
+=======
+// When the user clicks the button, open the modal 
+function openBox(id) {
+    if (id == "numberpad") {
+        document.getElementById("numberpadContainer").style.display = "block";
+        console.log("modal has been sucessfully opened");
+    }
+
+    if (id == "slider") {
+        shuffle();
+        document.getElementById("sliderContainer").style.display = "block";
+        console.log("modal has been successfully opened");
+    }
+>>>>>>> Stashed changes
 }
 
 // When the user clicks on <span> (x), close the modal
-function closethisshit() {
-    document.getElementById("numberpadContainer").style.display = "none";
+function closethisshit(id) {
+    if (id == "numberpad") {
+        document.getElementById("numberpadContainer").style.display = "none";
+    }
+    if (id == "slider") {
+        document.getElementById("sliderContainer").style.display = "none"
+    }
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
+    if (event.target == document.getElementById("numberpadContainer")) {
         document.getElementById("numberpadContainer").style.display = "none";
+
+    }
+    if (event.target == document.getElementById("sliderContainer")) {
+        document.getElementById("sliderContainer").style.display = "none"
     }
 }
+<<<<<<< Updated upstream
+=======
+
+function goToGarage() {
+    window.location.href = "./garage.html";
+}
+<<<<<<< Updated upstream
 
 function goToGarage(){
     window.location.href = "./garage.html";
 }
+=======
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
